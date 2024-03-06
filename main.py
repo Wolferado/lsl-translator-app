cf6311378d35568776bb617f58db5c94707fc64d
Aleksey Karelin <akarelins125@gmail.com>
2024-03-06 02:21:25 +0200

    Teaching base add

--- main.py
+++ main.py
@@ -9,44 +9,45 @@ mp_face_mesh = mp.solutions.face_mesh # Load the solution from mediapipe library
 cap = cv2.VideoCapture(0)
 
 # Create a mask for the hands
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8, max_num_hands=2) as hands, mp_face_mesh.FaceMesh(min_detection_confidence=0.5, max_num_faces=1) as face_mesh:
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=2) as hands, mp_face_mesh.FaceMesh(min_detection_confidence=0.5, max_num_faces=1) as face_mesh:
     while cap.isOpened():
-        ret, frame = cap.read()
-
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Assign color palette
+        ret, image = cap.read()
         image = cv2.flip(image, 1) # Flip the stream
-        image.flags.writeable = False # Disable writeable flags
+
+        cv2.putText(image, "Module: Recognition", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
 
         hand_results = hands.process(image) 
         face_results = face_mesh.process(image)
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 
         mp_drawing = mp.solutions.drawing_utils # Enabling drawing utilities from MediaPipe library
         mp_drawing_styles = mp.solutions.drawing_styles
 
+        # Draw face landmarks, if any
         if face_results.multi_face_landmarks:
             for face in face_results.multi_face_landmarks:
+                cv2.putText(image, "Face detected", (10,50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
                 mp_drawing.draw_landmarks(
                     image = image,
                     landmark_list = face,
-                    connections = mp_face_mesh.FACEMESH_IRISES,
-                    landmark_drawing_spec = None, # mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1),
-                    connection_drawing_spec = mp_drawing_styles.get_default_face_mesh_iris_connections_style()
+                    connections = mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec = mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1),
+                    connection_drawing_spec = mp_drawing_styles.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=1)
                 )
 
-        # If there are any multi_hand_landmarks
+        # Draw hand landmarks, if any
         if hand_results.multi_hand_landmarks:
             for hand in hand_results.multi_hand_landmarks:
+                cv2.putText(image, "Hand detected", (10,70), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255), 2)
                 mp_drawing.draw_landmarks(
                     image = image, 
                     landmark_list = hand, 
                     connections = mp_hands.HAND_CONNECTIONS, # Draw landmarks and their connections based on image, hand shown 
-                    landmark_drawing_spec = mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=3, circle_radius=3), # Customize landmarks
-                    connection_drawing_spec = mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2) # Customize connections
+                    landmark_drawing_spec = mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=2), # Customize landmarks
+                    connection_drawing_spec = mp_drawing.DrawingSpec(color=(255, 0, 255), thickness=2, circle_radius=2) # Customize connections
                 ) 
 
-        # Open a window to show the hands
+        # Open a window with the app
         cv2.imshow('Latvian Sign Language Detection App', image)
         
         # Exit statement by the Esc key
