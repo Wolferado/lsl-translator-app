c7b9d5f84b9a2f88caab8868c22b95e76274dd5a
Aleksey Karelin <akarelins125@gmail.com>
2024-03-13 00:17:44 +0200

    Data collecting script implement, script rename

--- collect.py
+++ collect.py
@@ -42,26 +42,35 @@ def drawLandmarks(image, face_results, hand_results):
 # Parse both results variables.
 def extractResultsLandmarks(face_results, hand_results):
     face_points_pos = np.zeros(1404)
-    first_hand_points_pos = np.zeros(63)
-    second_hand_points_pos = np.zeros(63)
+    left_hand_points_pos = np.zeros(63)
+    right_hand_points_pos = np.zeros(63)
 
     # If face detected (finished)
-    if face_results:
+    if face_results.multi_face_landmarks:
         face_points_pos = np.array([[point.x, point.y, point.z] for point in face_results.multi_face_landmarks[0].landmark]).flatten()
 
-    # If hands detected (W.I.P.)
-    if hand_results:
-        if hand_results.multi_handedness[0].index == 0:
-            first_hand_points_pos = np.array([[point.x, point.y, point.z] for point in hand_results.multi_hand_landmarks[0].landmark]).flatten()
-        elif hand_results.multi_handedness.index == 1:
-            second_hand_points_pos = np.array([[point.x, point.y, point.z] for point in hand_results.multi_hand_landmarks[1].landmark]).flatten()
-        '''else:
-            first_hand_points_pos = np.array([[point.x, point.y, point.z] for point in hand_results.multi_hand_landmarks[0].landmark]).flatten()
-            second_hand_points_pos = np.array([[point.x, point.y, point.z] for point in hand_results.multi_hand_landmarks[1].landmark]).flatten()'''
-    
-    return np.concatenate([face_points_pos, first_hand_points_pos, second_hand_points_pos])
+    # If hands detected
+    # Note: Classification labels are set completely opposite, because in video they recognize it that way 
+    if hand_results.multi_hand_landmarks:
+        for hand in hand_results.multi_handedness:
+            if hand.classification[0].label == "Right": # Left hand
+                left_hand_points_pos = np.array([[point.x, point.y, point.z] for point in hand_results.multi_hand_landmarks[0].landmark]).flatten()
+            if hand.classification[0].label == "Left": # Right hand
+                right_hand_points_pos = np.array([[point.x, point.y, point.z] for point in hand_results.multi_hand_landmarks[0].landmark]).flatten()
 
-for file in os.listdir(learningDir):
+    '''
+    print("Face")
+    print(face_points_pos)
+    print("Left")
+    if left_hand_points_pos.all() != 0:
+        print(left_hand_points_pos)
+    print("Right")
+    if right_hand_points_pos.all() != 0:
+        print(right_hand_points_pos)
+    '''
+    return np.concatenate([face_points_pos, left_hand_points_pos, right_hand_points_pos])
+
+for file in os.listdir(learningDir): # If there is a file in the directory
     if exitKeyPressed: # Check if need to break (useful to prevent all recordings from being learned)
         break
 
@@ -75,7 +84,7 @@ for file in os.listdir(learningDir):
     current_frame = 0 # Current frame
 
     # Create a mask for the hands and face
-    with mp_hands.Hands(min_detection_confidence=0.3, min_tracking_confidence=0.5, max_num_hands=2) as hands, mp_face_mesh.FaceMesh(min_detection_confidence=0.4, max_num_faces=1) as face_mesh:
+    with mp_hands.Hands(min_detection_confidence=0.9, min_tracking_confidence=0.5, max_num_hands=2) as hands, mp_face_mesh.FaceMesh(min_detection_confidence=0.4, max_num_faces=1) as face_mesh:
         
         origPath = "{}/{}".format(dataPath, "orig")
         flipPath = "{}/{}".format(dataPath, "flip")
@@ -85,6 +94,7 @@ for file in os.listdir(learningDir):
         # Save original video data
         while cap.isOpened():
             ret, image = cap.read()
+            image = cv2.flip(image, 1)
             cv2.putText(image, "Module: Teaching", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
 
             if ret == False: # If video reached its end
@@ -98,9 +108,9 @@ for file in os.listdir(learningDir):
             hand_results = hands.process(image) 
             face_results = face_mesh.process(image)
 
-            if hand_results:
-                #data = extractResultsLandmarks(face_results, hand_results)
-                #np.save("{}/{}".format(origPath, current_frame), data)
+            if hand_results.multi_hand_landmarks:
+                data = extractResultsLandmarks(face_results, hand_results)
+                np.save("{}/{}".format(origPath, current_frame), data)
                 current_frame += 1
 
             drawLandmarks(image, face_results, hand_results)
@@ -120,7 +130,6 @@ for file in os.listdir(learningDir):
         while cap.isOpened():
 
             ret, image = cap.read()
-            image = cv2.flip(image, 1)
             cv2.putText(image, "Module: Teaching", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
 
             if ret == False: # If video reached its end
@@ -130,9 +139,9 @@ for file in os.listdir(learningDir):
             hand_results = hands.process(image) 
             face_results = face_mesh.process(image)
 
-            if hand_results:
-                #data = extractResultsLandmarks(face_results, hand_results)
-                #np.save("{}/{}".format(flipPath, current_frame), data)
+            if hand_results.multi_hand_landmarks:
+                data = extractResultsLandmarks(face_results, hand_results)
+                np.save("{}/{}".format(flipPath, current_frame), data)
                 current_frame += 1
 
             drawLandmarks(image, face_results, hand_results)
@@ -148,3 +157,5 @@ for file in os.listdir(learningDir):
     # Turn off the webcam and close all program-connected windows
     cap.release()
     cv2.destroyAllWindows()
+
+print("Data about face's and hands' landmarks collected.")
--- main.py
+++ main.py
@@ -9,16 +9,17 @@ mp_face_mesh = mp.solutions.face_mesh # Load the solution from mediapipe library
 cap = cv2.VideoCapture(0)
 
 # Create a mask for the hands
-with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=2) as hands, mp_face_mesh.FaceMesh(min_detection_confidence=0.5, max_num_faces=1) as face_mesh:
+with mp_hands.Hands(min_detection_confidence=0.9, min_tracking_confidence=0.5, max_num_hands=2) as hands, mp_face_mesh.FaceMesh(min_detection_confidence=0.5, max_num_faces=1) as face_mesh:
     while cap.isOpened():
         ret, image = cap.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.flip(image, 1) # Flip the stream
 
         cv2.putText(image, "Module: Recognition", (10,30), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
 
         hand_results = hands.process(image) 
         face_results = face_mesh.process(image)
-
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         mp_drawing = mp.solutions.drawing_utils # Enabling drawing utilities from MediaPipe library
         mp_drawing_styles = mp.solutions.drawing_styles
@@ -46,6 +47,8 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, m
                     landmark_drawing_spec = mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=2), # Customize landmarks
                     connection_drawing_spec = mp_drawing.DrawingSpec(color=(255, 0, 255), thickness=2, circle_radius=2) # Customize connections
                 ) 
+            for hand in hand_results.multi_handedness:
+                print(hand.classification[0].label)
 
         # Open a window with the app
         cv2.imshow('Latvian Sign Language Detection App', image)
