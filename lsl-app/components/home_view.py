c287a269d4b838d152904ee8a5e7be41bf471d39
Aleksey Karelin <akarelins125@gmail.com>
2024-03-17 23:46:30 +0200

    Fixed NavigationBar, changed structure comparing to main

--- lsl-app/components/home_view.py
+++ lsl-app/components/home_view.py
@@ -2,11 +2,20 @@ import flet as ft
 
 class Home(ft.UserControl):
     def build(self):
-        self.container = ft.Column(
-            controls=[
-                ft.Text(value='Testing', size=30)
-            ]
+        self.app_title = ft.Container(
+            content=ft.Text('Latvian Sign Language Recognition App', text_align=ft.TextAlign.CENTER, size=32), 
+            padding=10, 
+            alignment=ft.alignment.center
+        )
+        self.app_desc = ft.Container(
+            content=ft.Text('Translate signs into text!', text_align=ft.TextAlign.CENTER, size=24), 
+            padding=10, 
+            alignment=ft.alignment.center
         )
 
-        return self.container
+        return (
+            ft.Column(
+                controls=[self.app_title, self.app_desc]
+            )
+        )
         
--- lsl-app/main.py
+++ lsl-app/main.py
@@ -1,15 +1,42 @@
 import flet as ft
-from components.app import LSLApp
+
+from components.home_view import Home
+from components.recognition_view import Recognition
+from components.model_teaching_view import Teaching
 
 def main(page: ft.Page):
     page.title = "LSL Prototype"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.update()
 
-    # create application instance
-    lsl_app = LSLApp()
+    view_placeholder = ft.Column(
+        controls=[Home()]
+    )
 
-    # add application's root control to the page
-    page.add(lsl_app)
+    def change_view(e):
+        view_placeholder.controls.clear()
+
+        if navigation_bar.selected_index == 0:
+            print("home_screen")
+            home_view = Home()
+            view_placeholder.controls.append(home_view)
+        elif navigation_bar.selected_index == 1:
+            print("recognize")
+            recognition_view = Recognition()
+            view_placeholder.controls.append(recognition_view)
+        elif navigation_bar.selected_index == 2:
+            print("teach")
+            teaching_view = Teaching()
+            view_placeholder.controls.append(teaching_view)
+
+        page.update()
+
+    navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.HOME, selected_icon=ft.icons.HOME_OUTLINED, label="Home Screen"),
+            ft.NavigationDestination(icon=ft.icons.CAMERA, selected_icon=ft.icons.CAMERA_OUTLINED, label="Recognize"),
+            ft.NavigationDestination(icon=ft.icons.MODEL_TRAINING, selected_icon=ft.icons.MODEL_TRAINING_SHARP, label="Model Teaching")
+        ], on_change=change_view
+    )
+
+    page.add(view_placeholder, navigation_bar)
 
 ft.app(target=main)
